b00033cf6dee594e54857dcf52ea5ac9b8e9315b
Max Gonzih <gonzih@gmail.com>
2013-10-28 20:02:39 +0300

    Implement submit-problem without global driver

--- src/euler_clojure/util.clj
+++ src/euler_clojure/util.clj
@@ -16,35 +16,35 @@
 (defn euler-pass []
   (System/getenv "EULER_PASS"))
 
-(defn login []
-  (to "https://projecteuler.net/login")
-  (input-text "#username" (euler-login))
-  (-> (find-element {:name "password"})
-      (input-text (euler-pass)))
-  (-> (find-element {:name "login"})
-      click))
+(defn login [driver]
+  (to driver "https://projecteuler.net/login")
+  (input-text driver "#username" (euler-login))
+  (input-text driver
+              (find-element driver {:name "password"})
+              (euler-pass))
+  (click driver (find-element driver {:name "login"})))
 
-(defn wait-fn []
-  (or (find-element {:tag :img :title "Wrong"})
-      (find-element {:tag :img :title "Correct"})))
+(defn wait-fn [driver]
+  (or (find-element driver {:tag :img :title "Wrong"})
+      (find-element driver {:tag :img :title "Correct"})))
 
 (defn show-problem [id]
   (to (new-driver {:browser :firefox})
       (str "https://projecteuler.net/problem=" id)))
 
 (defn submit-problem [id]
-  (let [driver (future (new-driver {:browser :firefox}))
-        answer (run-problem id)]
-    (set-driver! @driver)
-    (login)
-    (to (str "https://projecteuler.net/problem=" id))
-    (if (find-element {:id "guess"})
-      (do (input-text "#guess" answer)
-          (input-text (find-element {:name "confirm"}) ""))
-      (do (quit)
+  (let [driver-atom (future (new-driver {:browser :firefox}))
+        answer (run-problem id)
+        driver @driver-atom]
+    (login driver)
+    (to driver (str "https://projecteuler.net/problem=" id))
+    (if (find-element driver {:id "guess"})
+      (do (input-text driver "#guess" answer)
+          (input-text driver (find-element driver {:name "confirm"}) ""))
+      (do (quit driver)
           (throw (Exception. "Can't find input for answer. Already solved?"))))
-    (wait-until wait-fn 600000)
-    (condp = (attribute (wait-fn) "title")
+    (wait-until driver wait-fn 600000 1000)
+    (condp = (attribute driver (wait-fn driver) "title")
       "Wrong" (println "Wrong answer.")
       "Correct" (println "Good job!"))
-    (quit)))
+    (quit driver)))
